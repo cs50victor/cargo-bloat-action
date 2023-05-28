ed553c7c309020ef12cf5bb0a96864ea6c85c40b
Victor A <52110451+cs50victor@users.noreply.github.com>
2023-05-28 14:12:41 -0400

    fix(snapshot): replace old snapshot storage url

--- src/comments.ts
+++ src/comments.ts
@@ -1,160 +1,143 @@
-import * as github from '@actions/github'
-import {GitHub} from "@actions/github/lib/utils";
-import {context} from '@actions/github'
-import * as core from '@actions/core'
-import {SnapshotDifference} from './snapshots'
-import fileSize from 'filesize'
-import table from 'text-table'
-import {shouldIncludeInDiff} from "./utils"
+import * as github from "@actions/github";
+import { context } from "@actions/github";
+import * as core from "@actions/core";
+import { SnapshotDifference } from "./snapshots";
+import { partial } from "filesize";
+import table from "text-table";
+import { shouldIncludeInDiff } from "./utils";
 
-export function githubClient(): InstanceType<typeof GitHub> {
-  const token = core.getInput('token')
-  return github.getOctokit(token)
+const binarySize = (size: number) => partial({ standard: "jedec", output: "string" })(size) as string;
+
+export function githubClient() {
+  const token = core.getInput("token");
+  return github.getOctokit(token);
 }
 
-async function postNewComment(message: string): Promise<void> {
-  const client = githubClient()
-  await client.issues.createComment({
+async function postNewComment(message: string, client: ReturnType<typeof githubClient>): Promise<void> {
+  await client.rest.issues.createComment({
     body: message,
     issue_number: context.issue.number,
     owner: context.issue.owner,
-    repo: context.issue.repo
-  })
+    repo: context.issue.repo,
+  });
 }
 
-async function updateComment(
-  message: string,
-  comment_id: number
-): Promise<void> {
-  const client = githubClient()
-
-  await client.issues.updateComment({
+async function updateComment(message: string, comment_id: number, client: ReturnType<typeof githubClient>): Promise<void> {
+  await client.rest.issues.updateComment({
     body: message,
     comment_id,
     owner: context.issue.owner,
-    repo: context.issue.repo
-  })
+    repo: context.issue.repo,
+  });
 }
 
-export async function createOrUpdateComment(
-  toolchain: string,
-  message: string
-): Promise<void> {
-  core.info(`Find comments for issue: ${github.context.issue.number}`)
-  const client = githubClient()
+export async function createOrUpdateComment(toolchain: string, message: string): Promise<void> {
+  core.info(`Find comments for issue: ${github.context.issue.number}`);
+  const client = githubClient();
 
-  const comments = await client.issues.listComments({
+  // client.rest.pulls.
+  const comments = await client.rest.issues.listComments({
     owner: context.issue.owner,
     repo: context.issue.repo,
     issue_number: context.issue.number,
-    per_page: 100
-  })
+    per_page: 100,
+  });
 
   if (comments.status != 200) {
-    return core.setFailed(
-      `Error fetching comments for MR ${github.context.issue.number}`
-    )
+    return core.setFailed(`Error fetching comments for MR ${github.context.issue.number}`);
   }
-  core.info(
-    `Found ${comments.data.length} comments. Searching for comments containing ${toolchain}`
-  )
+  core.info(`Found ${comments.data.length} comments. Searching for comments containing ${toolchain}`);
 
-  const ourComments = comments.data.filter(v => {
+  const ourComments = comments.data.filter((v) => {
     // Is there a better way to do this?
-    return v.user.login == 'github-actions[bot]' && v.body.includes(toolchain)
-  })
+    return v.user?.login == "github-actions[bot]" && v.body?.includes(toolchain);
+  });
 
   if (!ourComments.length) {
-    core.info('No existing comment found, creating a new comment')
-    await postNewComment(message)
+    core.info("No existing comment found, creating a new comment");
+    await postNewComment(message, client);
   } else {
     // Update the first comment
-    const id = ourComments[0].id
-    core.info(`Updating comment with ID ${id}`)
-    await updateComment(message, id)
+    const id = ourComments[0].id;
+    core.info(`Updating comment with ID ${id}`);
+    await updateComment(message, id, client);
   }
 }
 
-export function createSnapshotComment(
-  diff: SnapshotDifference
-): string {
-  const crateTableRows: Array<[string, string]> = []
-  diff.crateDifference.forEach(d => {
+export function createSnapshotComment(diff: SnapshotDifference): string {
+  const crateTableRows: Array<[string, string]> = [];
+  diff.crateDifference.forEach((d) => {
     if (d.old === null && d.new === null) {
-      return
+      return;
     }
     if (d.old === d.new) {
-      crateTableRows.push([`${d.name}`, fileSize(d.new as number)])
+      crateTableRows.push([`${d.name}`, binarySize(d.new as number)]);
     } else {
       if (d.old) {
-        crateTableRows.push([`- ${d.name}`, fileSize(d.old)])
+        crateTableRows.push([`- ${d.name}`, binarySize(d.old)]);
       }
       if (d.new) {
-        crateTableRows.push([`+ ${d.name}`, fileSize(d.new)])
+        crateTableRows.push([`+ ${d.name}`, binarySize(d.new)]);
       }
     }
-  })
+  });
 
-  const sizeTableRows: Array<[string, string, string]> = []
+  const sizeTableRows: Array<[string, string, string]> = [];
   if (shouldIncludeInDiff(diff.currentSize, diff.oldSize)) {
-    sizeTableRows.push(['- Size', fileSize(diff.oldSize), ''])
-    sizeTableRows.push([
-      '+ Size',
-      `${fileSize(diff.currentSize)}`,
-      `${diff.sizeDifference > 0 ? '+' : ''}${fileSize(diff.sizeDifference)}`
-    ])
+    sizeTableRows.push(["- Size", binarySize(diff.oldSize), ""]);
+    sizeTableRows.push(["+ Size", `${binarySize(diff.currentSize)}`, `${diff.sizeDifference > 0 ? "+" : ""}${binarySize(diff.sizeDifference)}`]);
   } else {
-    sizeTableRows.push(['Size', fileSize(diff.currentTextSize), ''])
+    sizeTableRows.push(["Size", binarySize(diff.currentTextSize), ""]);
   }
 
   if (shouldIncludeInDiff(diff.currentTextSize, diff.oldTextSize)) {
-    sizeTableRows.push(['- Text Size', fileSize(diff.oldTextSize), ''])
-    sizeTableRows.push([
-      '+ Text Size',
-      `${fileSize(diff.currentTextSize)}`,
-      `${diff.textDifference > 0 ? '+' : ''}${fileSize(diff.textDifference)}`
-    ])
+    sizeTableRows.push(["- Text Size", binarySize(diff.oldTextSize), ""]);
+    sizeTableRows.push(["+ Text Size", `${binarySize(diff.currentTextSize)}`, `${diff.textDifference > 0 ? "+" : ""}${binarySize(diff.textDifference)}`]);
   } else {
-    sizeTableRows.push(['Text size', fileSize(diff.currentTextSize), ''])
+    sizeTableRows.push(["Text size", binarySize(diff.currentTextSize), ""]);
   }
 
-  const crateTable = table(crateTableRows)
+  const crateTable = table(crateTableRows);
 
-  const sizeTable = table(sizeTableRows)
+  const sizeTable = table(sizeTableRows);
 
+  let treeDiff;
 
-  let treeDiff
-
-  if (typeof diff.treeDiff === 'string') {
-    treeDiff = diff.treeDiff
+  if (typeof diff.treeDiff === "string") {
+    treeDiff = diff.treeDiff;
   } else {
-    const treeDiffLines: Array<string> = []
+    const treeDiffLines: Array<string> = [];
 
-    diff.treeDiff.forEach(change => {
-      let prefix = " "
+    diff.treeDiff.forEach((change) => {
+      let prefix = " ";
       if (change.added) {
-        prefix = "+"
+        prefix = "+";
       } else if (change.removed) {
-        prefix = "-"
+        prefix = "-";
       }
-      const splitLines = change.value.split("\n")
-      treeDiffLines.push(splitLines.slice(0, -1).map(line=>`${prefix} ${line}`).join("\n") + "\n")
-    })
+      const splitLines = change.value.split("\n");
+      treeDiffLines.push(
+        splitLines
+          .slice(0, -1)
+          .map((line) => `${prefix} ${line}`)
+          .join("\n") + "\n"
+      );
+    });
 
-    treeDiff = treeDiffLines.join('') + '\n'
+    treeDiff = treeDiffLines.join("") + "\n";
   }
 
-  let dependencyCountDiff
+  let dependencyCountDiff;
   if (diff.oldDependenciesCount == diff.newDependenciesCount) {
-    dependencyCountDiff = `Count: ${diff.oldDependenciesCount}`
+    dependencyCountDiff = `Count: ${diff.oldDependenciesCount}`;
   } else {
-    dependencyCountDiff = `- Count: ${diff.oldDependenciesCount}\n`
-    dependencyCountDiff += `+ Count: ${diff.newDependenciesCount}`
+    dependencyCountDiff = `- Count: ${diff.oldDependenciesCount}\n`;
+    dependencyCountDiff += `+ Count: ${diff.newDependenciesCount}`;
   }
 
   const crateDetailsText =
     crateTableRows.length == 0
-      ? 'No changes to crate sizes'
+      ? "No changes to crate sizes"
       : `
 <details>
 <summary>Size difference per crate</summary>
@@ -169,7 +152,7 @@ ${crateTable}
 \`\`\`
 
 </details>
-`
+`;
   const treeDiffText = `
 <details>
 <summary>Dependency tree</summary>
@@ -183,7 +166,7 @@ ${treeDiff}
 \`\`\`
 
 </details>
-`
+`;
 
   return `
 \`\`\`diff
@@ -196,48 +179,44 @@ ${sizeTable}
 ${crateDetailsText}
 
 ${treeDiffText}
-`
+`;
 }
 
-export function createComment(masterCommit: string | null, currentCommit: string,
-                              toolchain: string,
-                              snapshots: SnapshotDifference[]): string {
+export function createComment(masterCommit: string | null, currentCommit: string, toolchain: string, snapshots: SnapshotDifference[]): string {
   const emojiList = {
-    apple: 'apple',
-    windows: 'office',
-    arm: 'muscle',
-    linux: 'cowboy_hat_face' // Why not?
-  }
+    apple: "apple",
+    windows: "office",
+    arm: "muscle",
+    linux: "cowboy_hat_face", // Why not?
+  };
 
-  let selectedEmoji = 'crab'
+  let selectedEmoji = "crab";
 
   for (const [key, emoji] of Object.entries(emojiList)) {
     if (toolchain.includes(key)) {
-      selectedEmoji = emoji
-      break
+      selectedEmoji = emoji;
+      break;
     }
   }
 
-  const compareCommitText =
-    masterCommit == null
-      ? ''
-      : `([Compare with baseline commit](https://github.com/${context.repo.owner}/${context.repo.repo}/compare/${masterCommit}..${currentCommit}))`
+  const compareCommitText = masterCommit == null ? "" : `([Compare with baseline commit](https://github.com/${context.repo.owner}/${context.repo.repo}/compare/${masterCommit}..${currentCommit}))`;
 
-  let innerComment
+  let innerComment;
 
   if (snapshots.length == 1) {
-    innerComment = createSnapshotComment(snapshots[0])
+    innerComment = createSnapshotComment(snapshots[0]);
   } else {
-    innerComment = snapshots.map(snapshot => {
-      const comment = createSnapshotComment(snapshot)
-      return `<details>
+    innerComment = snapshots
+      .map((snapshot) => {
+        const comment = createSnapshotComment(snapshot);
+        return `<details>
 <summary><strong>${snapshot.packageName}</strong>${shouldIncludeInDiff(snapshot.currentSize, snapshot.oldSize) ? " (Changes :warning:)" : ""}</summary>
 <br />
 ${comment}
-</details>`
-    }).join('\n')
+</details>`;
+      })
+      .join("\n");
   }
-
 
   return `
   :${selectedEmoji}: Cargo bloat for toolchain **${toolchain}** :${selectedEmoji}:
@@ -245,5 +224,5 @@ ${comment}
   ${innerComment}
 
   Commit: ${currentCommit} ${compareCommitText}
-  `
+  `;
 }
--- src/main.ts
+++ src/main.ts
@@ -1,73 +1,49 @@
-import * as core from '@actions/core'
-import * as github from '@actions/github'
-import {
-  compareSnapshots,
-  Snapshot,
-  fetchSnapshot,
-  recordSnapshot
-} from './snapshots'
-import {
-  BloatOutput, CargoPackage, getCargoPackages,
-  getToolchainVersions,
-  installCargoDependencies, Package,
-  runCargoBloat, runCargoTree,
-  Versions
-} from './bloat'
+import axios from "axios";
+import * as core from "@actions/core";
+import * as github from "@actions/github";
+import { compareSnapshots, Snapshot, fetchSnapshot, recordSnapshot } from "./snapshots";
+import { BloatOutput, CargoPackage, getCargoPackages, getToolchainVersions, installCargoDependencies, Package, runCargoBloat, runCargoTree, Versions } from "./bloat";
 // import {createOrUpdateComment, createSnapshotComment} from './comments'
-import * as io from "@actions/io"
-import {createComment, createOrUpdateComment, createSnapshotComment} from "./comments"
+import * as io from "@actions/io";
+import { createComment, createOrUpdateComment, createSnapshotComment } from "./comments";
 
-const ALLOWED_EVENTS = ['pull_request', 'push']
+const ALLOWED_EVENTS = ["pull_request", "push"];
+
+const KV_URL = "https://known-crane-36141.kv.vercel-storage.com";
 
 async function run(): Promise<void> {
   if (!ALLOWED_EVENTS.includes(github.context.eventName)) {
-    core.setFailed(
-      `This can only be used with the following events: ${ALLOWED_EVENTS.join(
-        ', '
-      )}`
-    )
-    return
+    core.setFailed(`This can only be used with the following events: ${ALLOWED_EVENTS.join(", ")}`);
+    return;
   }
 
-  const cargoPath: string = await io.which('cargo', true)
+  const cargoPath: string = await io.which("cargo", true);
 
-  await core.group('Installing cargo dependencies', async () => {
-    await installCargoDependencies(cargoPath)
-  })
+  await core.group("Installing cargo dependencies", async () => {
+    await installCargoDependencies(cargoPath);
+  });
 
-  const versions = await core.group(
-    'Toolchain info',
-    async (): Promise<Versions> => {
-      return getToolchainVersions()
-    }
-  )
+  const versions = await core.group("Toolchain info", async (): Promise<Versions> => {
+    return getToolchainVersions();
+  });
 
-  const packages = await core.group(
-    'Inspecting cargo packages',
-    async (): Promise<Array<CargoPackage>> => {
-      return await getCargoPackages(cargoPath)
-    }
-  )
+  const packages = await core.group("Inspecting cargo packages", async (): Promise<Array<CargoPackage>> => {
+    return await getCargoPackages(cargoPath);
+  });
 
-  const packageData : Record<string, Package> = {}
+  const packageData: Record<string, Package> = {};
 
   for (const cargoPackage of packages) {
-    const bloatData = await core.group(
-      `Running cargo-bloat on package ${cargoPackage.name}`,
-      async (): Promise<BloatOutput> => {
-        return await runCargoBloat(cargoPath, cargoPackage.name)
-      }
-    )
-    const treeData = await core.group(
-      `Running cargo-tree on package ${cargoPackage.name}`,
-      async (): Promise<string> => {
-        return await runCargoTree(cargoPath, cargoPackage.name)
-      }
-    )
-    packageData[cargoPackage.name] = {bloat: bloatData, tree: treeData}
+    const bloatData = await core.group(`Running cargo-bloat on package ${cargoPackage.name}`, async (): Promise<BloatOutput> => {
+      return await runCargoBloat(cargoPath, cargoPackage.name);
+    });
+    const treeData = await core.group(`Running cargo-tree on package ${cargoPackage.name}`, async (): Promise<string> => {
+      return await runCargoTree(cargoPath, cargoPackage.name);
+    });
+    packageData[cargoPackage.name] = { bloat: bloatData, tree: treeData };
   }
 
-  const repo_path = `${github.context.repo.owner}/${github.context.repo.repo}`
+  const repo_path = `${github.context.repo.owner}/${github.context.repo.repo}`;
 
   const currentSnapshot: Snapshot = {
     commit: github.context.sha,
@@ -75,46 +51,44 @@ async function run(): Promise<void> {
     rustc: versions.rustc,
     bloat: versions.bloat,
     packages: packageData,
-  }
+  };
 
-  if (github.context.eventName == 'push') {
+  const _axios = axios.create({
+    baseURL: KV_URL,
+    headers: {
+      Authorization: `Bearer ${core.getInput("kv_token")}`,
+    },
+  });
+
+  if (github.context.eventName == "push") {
     // Record the results
-    return await core.group('Recording', async () => {
-      return await recordSnapshot(repo_path, currentSnapshot)
-    })
+    return await core.group("Recording", async () => {
+      return await recordSnapshot(_axios, repo_path, currentSnapshot);
+    });
   }
 
   // A merge request
-  const masterSnapshot = await core.group(
-    'Fetching last build',
-    async (): Promise<Snapshot | null> => {
-      return await fetchSnapshot(repo_path, versions.toolchain)
-    }
-  )
-  await core.group(
-    'Posting comment',
-    async (): Promise<void> => {
-      const masterCommit = masterSnapshot?.commit || null;
-      const snapShotDiffs = Object.entries(currentSnapshot.packages).map(obj => {
-        const [name, currentPackage] = obj
-        return compareSnapshots(name, masterCommit, currentPackage, masterSnapshot?.packages?.[name] || null)
-      })
-      core.info(`snapshot: ${JSON.stringify(snapShotDiffs, undefined, 2)}`)
-      const comment = createComment(masterCommit, currentSnapshot.commit, versions.toolchain, snapShotDiffs);
-      await createOrUpdateComment(
-        versions.toolchain,
-        comment
-      )
-    }
-  )
+  const masterSnapshot = await core.group("Fetching last build", async (): Promise<Snapshot | null> => {
+    return await fetchSnapshot(_axios, repo_path, versions.toolchain);
+  });
+  await core.group("Posting comment", async (): Promise<void> => {
+    const masterCommit = masterSnapshot?.commit || null;
+    const snapShotDiffs = Object.entries(currentSnapshot.packages).map((obj) => {
+      const [name, currentPackage] = obj;
+      return compareSnapshots(name, masterCommit, currentPackage, masterSnapshot?.packages?.[name] || null);
+    });
+    core.info(`snapshot: ${JSON.stringify(snapShotDiffs, undefined, 2)}`);
+    const comment = createComment(masterCommit, currentSnapshot.commit, versions.toolchain, snapShotDiffs);
+    await createOrUpdateComment(versions.toolchain, comment);
+  });
 }
 
 async function main(): Promise<void> {
   try {
-    await run()
-  } catch (error) {
-    core.setFailed(error.message)
+    await run();
+  } catch (error: any) {
+    core.setFailed(error?.message ?? error);
   }
 }
 
-main()
+main();
