48dc2562b0a68e9784d8fce393c879aa2757763d
Victor A <52110451+cs50victor@users.noreply.github.com>
2023-05-28 23:09:53 -0400

    fix(snapshot): replace old snapshot storage url (#1)
    - refactor(build): make release build optional
    - fix(snapshot): remove '/' from snapshot key
    - refactor(snapshot-key): improve check for default branch
    - refactor: update snapshot key logic
    - fix: pr message max limit

--- src/bloat.ts
+++ src/bloat.ts
@@ -1,110 +1,91 @@
-import * as exec from '@actions/exec'
-import * as core from '@actions/core'
-import {Crate} from './snapshots'
-import {captureOutput} from "./utils"
+import * as exec from "@actions/exec";
+import * as core from "@actions/core";
+import { Crate } from "./snapshots";
+import { captureOutput } from "./utils";
 
 export declare class Versions {
-  rustc: string
-  toolchain: string
-  bloat: string
+  rustc: string;
+  toolchain: string;
+  bloat: string;
 }
 
 export declare interface BloatOutput {
-  'file-size': number
-  'text-section-size': number
+  "file-size": number;
+  "text-section-size": number;
   // Yeah this makes no sense and is hacky as hell. Sue me.
-  crates: Array<Crate> | undefined,
-  functions: Array<Crate> | undefined
+  crates: Array<Crate> | undefined;
+  functions: Array<Crate> | undefined;
 }
 
 export declare interface Package {
-  bloat: BloatOutput
-  tree: string
+  bloat: BloatOutput;
+  tree: string;
 }
 
 export declare interface CargoPackage {
-  name: string
+  name: string;
 }
 
 declare interface CargoMetadata {
-  packages: Array<CargoPackage>
+  packages: Array<CargoPackage>;
 }
 
 export async function getToolchainVersions(): Promise<Versions> {
-  const toolchain_out = await captureOutput('rustup', [
-    'show',
-    'active-toolchain'
-  ])
-  const toolchain = toolchain_out.split(' ')[0]
+  const toolchain_out = await captureOutput("rustup", ["show", "active-toolchain"]);
+  const toolchain = toolchain_out.split(" ")[0];
 
-  const rustc_version_out = await captureOutput('rustc', ['--version'])
-  const rustc = rustc_version_out.split(' ')[1]
+  const rustc_version_out = await captureOutput("rustc", ["--version"]);
+  const rustc = rustc_version_out.split(" ")[1];
 
-  const bloat = (await captureOutput('cargo', ['bloat', '--version'])).trim()
-  const tree = (await captureOutput('cargo', ['tree', '--version'])).split(' ')[1].trim()
+  const bloat = (await captureOutput("cargo", ["bloat", "--version"])).trim();
+  const tree = (await captureOutput("cargo", ["tree", "--version"])).split(" ")[1].trim();
 
-  core.debug(
-    `Toolchain: ${toolchain} with rustc ${rustc}, cargo-bloat ${bloat} and cargo-tree ${tree}`
-  )
+  core.debug(`Toolchain: ${toolchain} with rustc ${rustc}, cargo-bloat ${bloat} and cargo-tree ${tree}`);
 
-  return {toolchain, bloat, rustc}
+  return { toolchain, bloat, rustc };
 }
 
 export async function installCargoDependencies(cargoPath: string): Promise<void> {
-  const args = ['install', 'cargo-bloat', 'cargo-tree', '--debug']
-  await exec.exec(cargoPath, args)
+  const args = ["install", "cargo-bloat", "cargo-tree", "--debug"];
+  await exec.exec(cargoPath, args);
 }
 
 export async function runCargoBloat(cargoPath: string, packageName: string): Promise<BloatOutput> {
-  const noCrates = core.getInput("by_function")
+  const noCrates = core.getInput("by_function");
 
-  const flags = [
-    '--release',
-    '--all-features',
-  ]
+  // const flags = ["--release", "--all-features"];
+  const flags = ["--all-features"];
 
   if (!noCrates) {
-    flags.push('--crates')
+    flags.push("--crates");
   }
 
-  let bloatArgs = [
-    'bloat',
-    ...flags,
-    '-p',
-    packageName
-  ]
-  let optionalArgs = core.getInput("bloat_args")
+  let bloatArgs = ["bloat", ...flags, "-p", packageName];
+  let optionalArgs = core.getInput("bloat_args");
 
   if (optionalArgs.length > 0) {
-    bloatArgs = ["bloat", ...optionalArgs.split(' ')]
+    bloatArgs = ["bloat", ...optionalArgs.split(" ")];
   }
-  bloatArgs.push('--message-format=json', '-n', '0')
-  const output = await captureOutput(cargoPath, bloatArgs)
-  return JSON.parse(output)
+  bloatArgs.push("--message-format=json", "-n", "0");
+  const output = await captureOutput(cargoPath, bloatArgs);
+  return JSON.parse(output);
 }
 
 export async function runCargoTree(cargoPath: string, packageName: string): Promise<string> {
   let optionalArgs = core.getInput("tree_args");
-  const args = (optionalArgs.length > 0) ? ['tree', ...optionalArgs.split(" ")] : [
-    'tree',
-    '--prefix-depth',
-    '--all-features',
-    '--no-dev-dependencies',
-    '-p',
-    packageName
-  ];
+  const args = optionalArgs.length > 0 ? ["tree", ...optionalArgs.split(" ")] : ["tree", "--prefix-depth", "--all-features", "--no-dev-dependencies", "-p", packageName];
   // The first line has the version and other metadata in it. We strip that here:
-  const lines = (await captureOutput(cargoPath, args)).split("\n")
-  return lines.slice(1).join("\n")
+  const lines = (await captureOutput(cargoPath, args)).split("\n");
+  return lines.slice(1).join("\n");
 }
 
 export async function getCargoPackages(cargoPath: string): Promise<Array<CargoPackage>> {
   const exclude_packages = core.getInput("exclude_packages").split(" ");
-  const args = ['metadata', '--no-deps', '--format-version=1']
-  const output = await captureOutput(cargoPath, args)
+  const args = ["metadata", "--no-deps", "--format-version=1"];
+  const output = await captureOutput(cargoPath, args);
   let result = (JSON.parse(output) as CargoMetadata).packages;
   if (exclude_packages.length > 0) {
-    result = result.filter(pack => !exclude_packages.includes(pack.name));
+    result = result.filter((pack) => !exclude_packages.includes(pack.name));
   }
   return result;
 }
--- src/snapshots.ts
+++ src/snapshots.ts
@@ -1,130 +1,122 @@
-import axios from 'axios'
-import * as core from '@actions/core'
-import {context} from '@actions/github'
-import * as Diff from 'diff'
-import {Change} from 'diff'
-import {Package} from "./bloat"
-import {shouldIncludeInDiff, treeToDisplay} from "./utils"
+import * as core from "@actions/core";
+import { context } from "@actions/github";
+import * as Diff from "diff";
+import { Change } from "diff";
+import { Package } from "./bloat";
+import { shouldIncludeInDiff, treeToDisplay } from "./utils";
+import { AxiosInstance } from "axios";
 
 declare interface CrateDifference {
-  name: string
+  name: string;
   // If old is null, it's a new crate. If new is null, it's been deleted.
   // If both have values then it's changed.
   // If both are null then something has gone terribly wrong.
-  old: number | null
-  new: number | null
+  old: number | null;
+  new: number | null;
 }
 
 export declare interface SnapshotDifference {
-  packageName: string
+  packageName: string;
 
-  currentSize: number
-  oldSize: number
-  sizeDifference: number
+  currentSize: number;
+  oldSize: number;
+  sizeDifference: number;
 
-  currentTextSize: number
-  oldTextSize: number
-  textDifference: number
+  currentTextSize: number;
+  oldTextSize: number;
+  textDifference: number;
 
-  masterCommit: string | null
-  currentCommit: string
+  masterCommit: string | null;
+  currentCommit: string;
 
-  crateDifference: Array<CrateDifference>
+  crateDifference: Array<CrateDifference>;
 
-  treeDiff: Change[] | string
-  oldDependenciesCount: number
-  newDependenciesCount: number
+  treeDiff: Change[] | string;
+  oldDependenciesCount: number;
+  newDependenciesCount: number;
 }
 
 export declare interface Crate {
-  crate: string | null
-  name: string
-  size: number
+  crate: string | null;
+  name: string;
+  size: number;
 }
 
 export declare interface Snapshot {
-  commit: string
-  toolchain: string
-  rustc: string
-  bloat: string
-  packages: Record<string, Package>
+  commit: string;
+  toolchain: string;
+  rustc: string;
+  bloat: string;
+  packages: Record<string, Package>;
 }
 
-
-function crateOrFunctionName(crate: Crate) : string {
-  const name = crate.crate ? `(${crate.crate}) ${crate.name}` : crate.name
+function crateOrFunctionName(crate: Crate): string {
+  const name = crate.crate ? `(${crate.crate}) ${crate.name}` : crate.name;
   if (name.length > 70) {
-    return `${name.substring(0, 70)}...`
+    return `${name.substring(0, 70)}...`;
   }
-  return name
+  return name;
 }
 
+export function compareSnapshots(packageName: string, masterCommit: string | null, current: Package, master: Package | null): SnapshotDifference {
+  const masterFileSize = master?.bloat["file-size"] || 0;
+  const masterTextSize = master?.bloat["text-section-size"] || 0;
 
-export function compareSnapshots(
-  packageName: string,
-  masterCommit: string | null,
-  current: Package,
-  master: Package | null
-): SnapshotDifference {
-  const masterFileSize = master?.bloat["file-size"] || 0
-  const masterTextSize = master?.bloat["text-section-size"]|| 0
+  const sizeDifference = current.bloat["file-size"] - masterFileSize;
+  const textDifference = current.bloat["text-section-size"] - masterTextSize;
 
-  const sizeDifference = current.bloat["file-size"] - masterFileSize
-  const textDifference = current.bloat["text-section-size"] - masterTextSize
-
-  const currentCratesObj: { [key: string]: number } = {}
-  const currentCrateOrFunction = current.bloat.crates ? current.bloat.crates : current.bloat.functions
-  const masterCrateOrFunction = master?.bloat.crates ? master?.bloat.crates : master?.bloat.functions
+  const currentCratesObj: { [key: string]: number } = {};
+  const currentCrateOrFunction = current.bloat.crates ? current.bloat.crates : current.bloat.functions;
+  const masterCrateOrFunction = master?.bloat.crates ? master?.bloat.crates : master?.bloat.functions;
 
   // Should never happen
   if (currentCrateOrFunction == undefined) {
-    throw Error("Neither crates or functions are defined!")
+    throw Error("Neither crates or functions are defined!");
   }
 
   for (const o of currentCrateOrFunction) {
-    currentCratesObj[crateOrFunctionName(o)] = o.size
+    currentCratesObj[crateOrFunctionName(o)] = o.size;
   }
-  const masterCratesObj: { [key: string]: number } = {}
+  const masterCratesObj: { [key: string]: number } = {};
   for (const o of masterCrateOrFunction || []) {
-    masterCratesObj[crateOrFunctionName(o)] = o.size
+    masterCratesObj[crateOrFunctionName(o)] = o.size;
   }
 
   // Ignore unknown crates for now.
-  delete currentCratesObj['[Unknown]']
-  delete masterCratesObj['[Unknown]']
+  delete currentCratesObj["[Unknown]"];
+  delete masterCratesObj["[Unknown]"];
 
-  const crateDifference: CrateDifference[] = []
+  const crateDifference: CrateDifference[] = [];
 
   // Crates with new or altered values
   for (const [name, newValue] of Object.entries(currentCratesObj)) {
-    let oldValue: number | null = masterCratesObj[name] || null
+    let oldValue: number | null = masterCratesObj[name] || null;
     if (oldValue == null) {
-      oldValue = null
+      oldValue = null;
     } else {
-      delete masterCratesObj[name]
+      delete masterCratesObj[name];
     }
     if (shouldIncludeInDiff(newValue, oldValue)) {
-      crateDifference.push({name, new: newValue, old: oldValue})
+      crateDifference.push({ name, new: newValue, old: oldValue });
     }
   }
 
   // Crates that have been removed
   for (const [name, oldValue] of Object.entries(masterCratesObj)) {
-    crateDifference.push({name, new: null, old: oldValue})
+    crateDifference.push({ name, new: null, old: oldValue });
   }
 
-  const currentSize = current.bloat["file-size"]
-  const currentTextSize = current.bloat["text-section-size"]
+  const currentSize = current.bloat["file-size"];
+  const currentTextSize = current.bloat["text-section-size"];
 
-  const oldSize = masterFileSize
-  const oldTextSize = masterTextSize
+  const oldSize = masterFileSize;
+  const oldTextSize = masterTextSize;
 
-  const treeDiff = master?.tree && master.tree !== current.tree ?
-    Diff.diffLines(treeToDisplay(master.tree), treeToDisplay(current.tree)) : treeToDisplay(current.tree)
-    // Diff.structuredPatch("master", "branch", treeToDisplay(master.tree), treeToDisplay(current.tree), "", "", {}).hunks : treeToDisplay(current.tree)
+  const treeDiff = master?.tree && master.tree !== current.tree ? Diff.diffLines(treeToDisplay(master.tree), treeToDisplay(current.tree)) : treeToDisplay(current.tree);
+  // Diff.structuredPatch("master", "branch", treeToDisplay(master.tree), treeToDisplay(current.tree), "", "", {}).hunks : treeToDisplay(current.tree)
 
-  const oldDependenciesCount = master?.tree.split("\n").length || 0
-  const newDependenciesCount = current.tree.split("\n").length
+  const oldDependenciesCount = master?.tree.split("\n").length || 0;
+  const newDependenciesCount = current.tree.split("\n").length;
 
   return {
     packageName,
@@ -139,31 +131,24 @@ export function compareSnapshots(
     currentCommit: context.sha,
     treeDiff,
     newDependenciesCount,
-    oldDependenciesCount
-  }
+    oldDependenciesCount,
+  };
 }
 
-export async function fetchSnapshot(
-  repo: string,
-  toolchain: string
-): Promise<Snapshot | null> {
-  // Don't be a dick, please.
-  const url = `https://us-central1-cargo-bloat.cloudfunctions.net/fetch`
-  const res = await axios.get(url, {params: {repo, toolchain}})
-  core.info(`Response: ${JSON.stringify(res.data)}`)
-  // This is a bit screwed.
-  if (Object.keys(res.data).length == 0) {
-    return null
-  }
-  return res.data as Snapshot
+export async function getMasterBranchSnapshot(axios: AxiosInstance, repo_name: string, toolchain: string): Promise<Snapshot | null> {
+  const key = snapshotKey(repo_name, toolchain) + suffix(true, "ref");
+  core.info(`Fetching snapshot with key - ${key}`);
+  const res = await axios.get(`/get/${key}`);
+  core.info(`Response: ${JSON.stringify(res.data)}`);
+  return (res?.data as Snapshot) ?? null;
 }
 
-export async function recordSnapshot(
-  repo: string,
-  snapshot: Snapshot
-): Promise<void> {
-  // Don't be a dick, please.
-  const url = `https://us-central1-cargo-bloat.cloudfunctions.net/ingest`
-  core.info(`Post data: ${JSON.stringify(snapshot, undefined, 2)}`)
-  await axios.post(url, snapshot, {params: {repo}})
+export async function saveSnapshot(axios: AxiosInstance, repo_name: string, snapshot: Snapshot, ref: string, is_default_branch: boolean): Promise<void> {
+  core.info(`Post data: ${JSON.stringify(snapshot, undefined, 2)}`);
+  const key = snapshotKey(repo_name, snapshot.toolchain) + suffix(is_default_branch, ref);
+  core.info(`Saving snapshot with key - ${key}`);
+  await axios.post(`/set/${key}`, snapshot);
 }
+
+const snapshotKey = (repo_name: string, toolchain: string) => `${repo_name}-${toolchain}`;
+const suffix = (is_default_branch: boolean, ref: string) => (is_default_branch ? "-main" : `-${ref}`);
